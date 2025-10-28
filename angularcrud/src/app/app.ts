8dc170abea6e90259a39655c884e27d11e2dfb05
smateusx <smateux10@gmail.com>
2025-10-27 22:16:02 -0300

    comecei o header

--- angularcrud/src/app/app.ts
+++ angularcrud/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./pages/home/home";
+import { Header } from "./shared/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
