ff32039392e1ba6dc8123598f6cf59b49a74d994
smateusx <smateux10@gmail.com>
2025-10-28 23:42:50 -0300

    att pagina login

--- angularcrud/src/app/pages/login/login.ts
+++ angularcrud/src/app/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.css',
 })
 export class Login {
-  login:string = '';
-  senha:string = '';
+  login: string = '';
+  senha: string = '';
 
-  onBotaoClicado():void{
-    
+  constructor(private router:Router){}
+
+  onBotaoClicado(): void {
+
+    if (this.login.trim() !== '' && this.senha.trim() !== '') {
+      if (this.login === 'admin'.trim() && this.senha.trim() === '123') {
+        this.router.navigate(['/pessoas'])
+      } else {
+        alert('Dados inválidos!')
+      }
+    } else {
+      alert('Preencha todos os campos!')
+    }
   }
 }
