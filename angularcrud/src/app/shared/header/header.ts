b5c20f5695599427794627c099f43b020a1a990a
smateusx <smateux10@gmail.com>
2025-10-29 00:00:34 -0300

    inciei login

--- angularcrud/src/app/shared/header/header.ts
+++ angularcrud/src/app/shared/header/header.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  exibirLogo: boolean = true;
 
+  itensMenu = [
+    {label: 'Home', link:''},
+    {label: 'Cadastro', link:'/pessoas/incluir'},
+    {label: 'Login', link:'/pessoas/login'},
+    {label: 'Sobre', link:'/pessoas/sobre'},
+    {label: 'Ajuda', link:'/pessoas/ajuda'},
+  ];
 }
